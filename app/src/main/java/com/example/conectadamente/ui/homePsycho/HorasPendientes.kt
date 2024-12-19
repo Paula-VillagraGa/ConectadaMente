6c62c254cb113ac13a3a8f0a3a049c17d046aa41
Paula <p.villagraga@gmail.com>
2024-12-19 16:10:56 -0300

    Detalles lista horas

--- app/src/main/java/com/example/conectadamente/ui/homePsycho/HorasPendientes.kt
+++ app/src/main/java/com/example/conectadamente/ui/homePsycho/HorasPendientes.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,6 +27,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -41,6 +44,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.conectadamente.ui.viewModel.calendar.AppointmentViewModel
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,65 +53,113 @@ fun ReservedAppointmentsScreen(
     viewModel: AppointmentViewModel,
     navController: NavController // Agregamos el controlador de navegación
 ) {
-    // Observar las citas pendientes desde el ViewModel
     val citasPendientes by viewModel.citasPendientes.observeAsState(emptyList())
     val errorMessage by viewModel.errorMessage.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState(false)
 
-    // Obtenemos las citas pendientes
+    var searchQuery by remember { mutableStateOf("") }
+
     LaunchedEffect(true) {
         viewModel.obtenerCitasPendientes("psychoIdExample") // Aquí puedes usar el psychoId real
     }
 
-    // Si hay un mensaje de error, lo mostramos
     if (errorMessage?.isNotEmpty() == true) {
-        Text(text = "Error: $errorMessage", color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(16.dp))
+        Text(
+            text = "Error: $errorMessage",
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(16.dp)
+        )
     }
 
-    // Contenido de la pantalla dentro de un Scaffold
+
+    val citasOrdenadas = citasPendientes.sortedBy { cita ->
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        LocalDate.parse(
+            cita.fechaHora.split(" ")[0],
+            formatter
+        )
+    }
+
+    val citasFiltradas = citasOrdenadas.filter { cita ->
+        cita.paciente?.contains(searchQuery, ignoreCase = true) == true
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text("Citas Pendientes", color = MaterialTheme.colorScheme.primary) },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) { // Acción de volver
-                        Icon(imageVector = Icons.Default.ArrowBackIosNew, contentDescription = "Back")
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = "Back"
+                        )
                     }
                 }
             )
         },
         content = { paddingValues ->
-            Box(modifier = Modifier.fillMaxSize()) {
-                // Si está cargando, mostrar el círculo de carga centrado
-                if (isLoading) {
-                    CircularProgressIndicator(
-                        color = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier
-                            .align(Alignment.Center) // Centrado en el centro de la pantalla
-                            .padding(16.dp)
-                    )
-                } else {
-                    LazyColumn(
-                        contentPadding = PaddingValues(
-                            start = 10.dp, // Margen izquierdo
-                            top = 16.dp,   // Margen superior
-                            end = 16.dp,   // Margen derecho
-                            bottom = 16.dp // Margen inferior
-                        ),
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(paddingValues) // Asegura que el contenido de LazyColumn no se corte
-                    ) {
-                        items(citasPendientes) { cita ->
-                            ReservedAppointmentCard(
-                                appointmentId = cita.appointmentId, // Pasamos el appointmentId
-                                fechaHora = cita.fechaHora,
-                                paciente = cita.paciente
-                            ) { id ->
-                                navController.navigate(
-                                    "editarCita/$id?fechaHora=${cita.fechaHora}&paciente=${cita.paciente ?: ""}"
-                                )
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+            ) {
+                // Campo de búsqueda
+                TextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    label = { Text("Buscar paciente") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Buscar",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)  // Padding horizontal para los bordes
+                        .padding(bottom = 8.dp),  // Ajusta el espacio debajo del campo de búsqueda
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Transparent,  // Fondo transparente
+                        focusedIndicatorColor = MaterialTheme.colorScheme.primary,  // No mostrar indicador al tener foco
+                        unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary  // No mostrar indicador cuando no tiene foco
+                    ),
+                    shape = MaterialTheme.shapes.medium  // Configura la forma, si lo deseas
+                )
 
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    if (isLoading) {
+                        CircularProgressIndicator(
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .padding(16.dp)
+                        )
+                    } else {
+                        LazyColumn(
+                            contentPadding = PaddingValues(
+                                start = 10.dp,
+                                top = 16.dp,
+                                end = 16.dp,
+                                bottom = 16.dp
+                            ),
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+                            items(citasFiltradas) { cita ->
+                                ReservedAppointmentCard(
+                                    appointmentId = cita.appointmentId, // Pasamos el appointmentId
+                                    fechaHora = cita.fechaHora,
+                                    paciente = cita.paciente
+                                ) { id ->
+                                    navController.navigate(
+                                        "editarCita/$id?fechaHora=${cita.fechaHora}&paciente=${cita.paciente ?: ""}"
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(8.dp))
                             }
                         }
                     }
@@ -115,6 +168,7 @@ fun ReservedAppointmentsScreen(
         }
     )
 }
+
 @Composable
 fun ReservedAppointmentCard(
     appointmentId: String, // Agregar el ID de la cita
@@ -128,15 +182,19 @@ fun ReservedAppointmentCard(
             .fillMaxWidth()
             .padding(bottom = 8.dp)
             .clickable { onCardClick(appointmentId) }, // Acción al hacer clic
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface // Aquí puedes elegir el color deseado
+        ),
+        elevation = CardDefaults.cardElevation(4.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             val (fecha, hora) = fechaHora.split(" ")
-            Text("Fecha: $fecha", color= MaterialTheme.colorScheme.primary, style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold))
+            Text("Fecha: $fecha", color= MaterialTheme.colorScheme.primary, style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold))
             Spacer(modifier = Modifier.height(4.dp))
             Text("Hora: $hora", color= MaterialTheme.colorScheme.secondary,style = MaterialTheme.typography.bodyMedium)
             Spacer(modifier = Modifier.height(8.dp))
-            Text("Paciente: ${paciente ?: "Paciente no disponible"}", color= MaterialTheme.colorScheme.secondary,style = MaterialTheme.typography.bodyMedium)
+            Text("Paciente: ${paciente ?: "Paciente no disponible"}", color= Color.Gray,style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
